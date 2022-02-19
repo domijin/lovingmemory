e9fc5c885aa696783781ccab3a2bae66e236a213
domijin <domijin@users.noreply.github.com>
2022-02-19 18:09:16 +0000

    deploy: 39260794365171399b490afc15083a4b42d4de30

---
+++ _nuxt/static/1645294155/charity-donation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645294155",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{name:"Jing Luo",dates:"Dec 11th 1987 - Feb 15th 2022"}}}("\u002Fcharity-donation"));
