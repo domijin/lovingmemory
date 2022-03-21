4f8e820e0a21e29e92554c48bbe70bbe84a53004
Domi Jin <contact@domij.info>
2022-03-21 00:53:17 -0700

    update obituary

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -9,15 +9,59 @@
       <!-- TO be updated
       <p class="highlight">A Bit of important text that you want to stand out, such as any dressing requirements.</p>
       -->
+      <h2>Obituary 讣告</h2>
+      <p>
+        It is with profound sadness that the family of Jing Luo announces his
+        passing on February 15th, 2022 at the age of 34. Forever joyful and
+        highly charismatic, Jing will be fondly remembered and missed by all
+        those who knew him.
+      </p>
+      <p>
+        2022年2月15日，骆靖（34岁）的家人怀着深切的悲痛宣布了他的逝世。骆靖总是乐观而充满魅力，所有认识他的人都会怀念他、怀念他。
+      </p>
+      <p>
+        Born in the late 1980’s in Taiyuan City, Shanxi Province, Jing was a
+        lively and lovable child. He did his undergraduate work in Applied
+        Physics at Zhejiang University of Technology in China. His strong work
+        ethic coupled with his general curiosity won him many honors and
+        scholarships.
+      </p>
+      <p>
+        骆靖于1980年代末出生于山西省太原市。他曾是个活泼可爱的孩子。他于中国浙江工业大学完成了应用物理专业的本科学习。他强烈的工作道德与精神和他对事物的好奇心为他赢得了无数荣誉和奖学金。
+      </p>
+      <p>
+        After being an outstanding student at Yunnan University’s Gravitational
+        Wave Summer School, he achieved his lifelong dream of going to the
+        United States when he was chosen to pursue advanced degrees in Physics
+        at the University of Texas Brownsville and the University of Texas at
+        San Antonio. After receiving his PhD in Physics, he went on to pursue
+        his postdoctoral research at the prestigious Canadian Institute of
+        Theoretical Astrophysics.
+      </p>
+      <p>
+        成为云南大学引力波暑期学校的优秀学生后，他前往美国德克萨斯大学布朗斯维尔分校和德克萨斯大学圣安东尼奥分校攻读物理学高等学位，实现了毕生梦想。在获得物理学博士学位后，他前往举世闻名的加拿大理论天体物理研究所从事博士后研究。
+      </p>
+      <p>
+        As much as he loved astrophysics, he loved life. His joyful and generous
+        nature touched all those he knew, bringing smiles and brightening days.
+        He was taken from us too soon, but his memory will live on. A service of
+        remembrance for the beloved “space cowboy” will be held at 1:00 PM on
+        March 21st at the Chapel Ridge Funeral Home in Markham, Ontario.
+      </p>
+      <p>
+        就像对天体物理学一样，他对生活也充满热爱。他快乐而慷慨的天性感动了所有他认识的人，为所有人带来了无数充满欢笑和阳光的日子。他过早离开了我们，但他的记忆会永存。
+        3 月 21 日下午
+        1:00，我们将在安大略省马克汉姆（Markham）市的教堂岭殡葬墓园（Chapel
+        Ridge Funeral Home）为人们深爱的“太空牛仔”举行纪念仪式。
+      </p>
+
       <h2>
-        Beloved son, student, colleague and friend -
-        亲爱的⼉⼦、学⽣、同事和朋友
+        Order of Service 仪式流程
         <span
           >Monday, March 21, 2022 at 1.00 p.m. - 2022年3月22日 1.00 a.m.
           星期二</span
         >
       </h2>
-      <h2>Order of Service 仪式流程</h2>
       <p>
         Jing’s family and friends would like to thank you for your support in
         honoring the life and legacy of Jing -
